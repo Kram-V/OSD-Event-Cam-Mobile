7686d3fbd68c4f8462d69e64461209e74bca3f68
Kram-V <markanthonyvivar24@gmail.com>
2025-05-04 16:25:46 +0800

    added design in home screen and integrated api

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,83 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const HomeScreen = () => {
+  const [stats, setStats] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://10.0.2.2:8000/api/mobile-stats")
+      .then((res) => {
+        setStats(res.data.total_non_admin_users);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  console.log(stats);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>HOME SCREEN</Text>
+      <View style={styles.totalStudentReports}>
+        <View>
+          <Icon
+            name="bar-chart"
+            size={30}
+            color="#E55353"
+            style={{ marginBottom: 10 }}
+          />
+
+          <Text>Total Student Reports</Text>
+        </View>
+
+        <Text style={styles.totalNumber}>50</Text>
+      </View>
+
+      <View style={styles.totalPendingReports}>
+        <View>
+          <Icon
+            name="hourglass-half"
+            size={30}
+            color="#F9B115"
+            style={{ marginBottom: 10 }}
+          />
+
+          <Text>Total Pending Reports</Text>
+        </View>
+
+        <Text style={styles.totalNumber}>25</Text>
+      </View>
+
+      <View style={styles.totalResolvedReports}>
+        <View>
+          <Icon
+            name="hourglass-half"
+            size={30}
+            color="#1B9E3E"
+            style={{ marginBottom: 10 }}
+          />
+
+          <Text>Total Resolved Reports</Text>
+        </View>
+
+        <Text style={styles.totalNumber}>25</Text>
+      </View>
+
+      <View style={styles.totalStaffUsers}>
+        <View>
+          <Icon
+            name="users"
+            size={30}
+            color="#3399FF"
+            style={{ marginBottom: 10 }}
+          />
+
+          <Text>Total Staff Users</Text>
+        </View>
+
+        <Text style={styles.totalNumber}>{stats}</Text>
+      </View>
     </View>
   );
 };
@@ -14,10 +87,64 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 30,
   },
+
   text: {
     fontSize: 30,
+  },
+
+  totalStudentReports: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 30,
+    borderRadius: 20,
+    elevation: 4,
+    backgroundColor: "#fff",
+    borderLeftWidth: 3,
+    borderLeftColor: "#E55353",
+    marginBottom: 20,
+  },
+
+  totalPendingReports: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 30,
+    borderRadius: 20,
+    elevation: 4,
+    backgroundColor: "#fff",
+    borderLeftWidth: 3,
+    borderLeftColor: "#F9B115",
+    marginBottom: 20,
+  },
+
+  totalResolvedReports: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 30,
+    borderRadius: 20,
+    elevation: 4,
+    backgroundColor: "#fff",
+    borderLeftWidth: 3,
+    borderLeftColor: "#1B9E3E",
+    marginBottom: 20,
+  },
+
+  totalStaffUsers: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 30,
+    borderRadius: 20,
+    elevation: 4,
+    backgroundColor: "#fff",
+    borderLeftWidth: 3,
+    borderLeftColor: "#3399FF",
+    marginBottom: 20,
+  },
+
+  totalNumber: {
+    fontSize: 40,
+    color: "#d3d3d3",
+    fontWeight: 600,
   },
 });
